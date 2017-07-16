811fd88bfb13cec312bc6c338f1c823edf3f8dee
Julián Unrrein <junrrein@gmail.com>
2017-07-16 00:59:52 -0300

    Agregar una función de CMake para agregar archivos de datos a un ejercicio

--- ejemplos/variosArchivos.cpp
+++ ejemplos/variosArchivos.cpp
@@ -1,8 +1,22 @@
 #include "funcion1.hpp"
 #include "funcion2.hpp"
+#include <armadillo>
+#include <gnuplot-iostream.h>
+
+using namespace arma;
 
 int main()
 {
     hacerAlgo();
     hacerOtraCosa();
+
+    vec datos;
+    datos.load("sin.txt");
+
+    Gnuplot gp;
+    gp << "plot " << gp.file1d(datos) << "with lines" << std::endl;
+
+    getchar();
+
+    return 0;
 }
