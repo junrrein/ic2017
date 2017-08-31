e6644d3863ca4bc526754eb6ab303507c15cce50
Julián Unrrein <junrrein@gmail.com>
2017-08-31 11:22:29 -0300

    Guia 1 - Ejercicio 1: No usar los datos de prueba durante el entrenamiento

--- guia1/ejercicio1.cpp
+++ guia1/ejercicio1.cpp
@@ -7,14 +7,15 @@
 using namespace std;
 using namespace arma;
 
-pair<vec, double> entrenarPerceptron(const mat& patronesEnt,
-                                     const mat& patronesPrueba,
-                                     const vec& salidaDeseadaEnt,
-                                     const vec& salidaDeseadaPrueba,
+pair<vec, double> entrenarPerceptron(const mat& patrones,
+                                     const vec& salidaDeseada,
                                      int nEpocas,
                                      double tasaAprendizaje,
                                      double tolerancia,
                                      string tituloGrafica);
+double errorPrueba(const vec& pesos,
+                   const mat& patrones,
+                   const vec& salidaDeseada);
 
 int main()
 {
@@ -31,15 +32,13 @@ int main()
     // OR
     // Entrenar la red graficando resultados intermedios (la recta)
     vec pesos;
-    double tasaError;
-    tie(pesos, tasaError) = entrenarPerceptron(patronesEntOR,
-                                               patronesPruebaOR,
-                                               salidaDeseadaEntOR,
-                                               salidaDeseadaPruebaOR,
-                                               100,
-                                               0.9,
-                                               5,
-                                               "OR");
+    tie(pesos, ignore) = entrenarPerceptron(patronesEntOR,
+                                            salidaDeseadaEntOR,
+                                            100,
+                                            0.1,
+                                            5,
+                                            "OR");
+    double tasaError = errorPrueba(pesos, patronesPruebaOR, salidaDeseadaPruebaOR);
     cout << "tasa de error del OR : " << tasaError << endl;
 
     // XOR
@@ -52,14 +51,13 @@ int main()
     mat patronesPruebaXOR = datos.head_cols(2);
     vec salidaDeseadaPruebaXOR = datos.tail_cols(1);
 
-    tie(pesos, tasaError) = entrenarPerceptron(patronesEntXOR,
-                                               patronesPruebaXOR,
-                                               salidaDeseadaEntXOR,
-                                               salidaDeseadaPruebaXOR,
-                                               100,
-                                               0.1,
-                                               5,
-                                               "XOR");
+    tie(pesos, ignore) = entrenarPerceptron(patronesEntXOR,
+                                            salidaDeseadaEntXOR,
+                                            100,
+                                            0.1,
+                                            5,
+                                            "XOR");
+    tasaError = errorPrueba(pesos, patronesPruebaXOR, salidaDeseadaPruebaXOR);
     cout << "tasa de error del XOR : " << tasaError << endl;
 
     return 0;
@@ -75,42 +73,49 @@ int sign(double numero)
 }
 }
 
-pair<vec, double> entrenarPerceptron(const mat& patronesEnt,
-                                     const mat& patronesPrueba,
-                                     const vec& salidaDeseadaEnt,
-                                     const vec& salidaDeseadaPrueba,
+pair<vec, double> entrenarPerceptron(const mat& patrones,
+                                     const vec& salidaDeseada,
                                      int nEpocas,
                                      double tasaAprendizaje,
                                      double tolerancia,
                                      string tituloGrafica)
 {
     // Inicializar pesos y tasa de error
-    vec pesos = randu<vec>(patronesEnt.n_cols + 1) - 0.5;
+    vec pesos = randu<vec>(patrones.n_cols + 1) - 0.5;
     double tasaError = 0;
 
-    // Extender la matriz de patrones con la entrada correspondiente al umbral
-    const mat patronesExt = join_horiz(ones(patronesEnt.n_rows) * (-1), patronesEnt);
+    // Extender las matrices de patrones con la entrada correspondiente al umbral
+    const mat patronesExt = join_horiz(ones(patrones.n_rows) * (-1), patrones);
 
-    // Separar patrones en los casos verdaderos y falsos
-    const mat verdaderos = patronesEnt.rows(find(salidaDeseadaEnt == 1));
-    const mat falsos = patronesEnt.rows(find(salidaDeseadaEnt == -1));
+    // Separar patrones de entrenamiento en casos verdaderos y falsos
+    // para graficarlos en colores distintos
+    const mat verdaderos = patrones.rows(find(salidaDeseada == 1));
+    const mat falsos = patrones.rows(find(salidaDeseada == -1));
 
+    // Objeto para graficar
+    Gnuplot gp;
     // Caracter usado para dejar de graficar
     const char caracterSalteo = 's';
     char caracter = ' ';
-    // Objeto para graficar
-    Gnuplot gp;
 
     // Ciclo de las epocas
     for (int epoca = 1; epoca <= nEpocas; ++epoca) {
         // Ciclo para una época
-        for (unsigned int i = 0; i < patronesEnt.n_rows; ++i) {
+        for (unsigned int i = 0; i < patrones.n_rows; ++i) {
             double z = dot(patronesExt.row(i), pesos);
             int y = ic::sign(z);
 
-            // Actualizar pesos tener en cuenta el orden en que colocamos la salida por la direccion del
-            // gradiente.
-            pesos += tasaAprendizaje * (salidaDeseadaEnt(i) - y) * patronesExt.row(i).t();
+            if (caracter != caracterSalteo) {
+                cout << "Patrón que entró: " << patrones.row(i)
+                     << "Salida para ese patrón: " << y << '\n'
+                     << "Salida deseada: " << salidaDeseada(i) << endl;
+
+                caracter = getchar();
+            }
+
+            // Actualizar pesos
+            // Tener en cuenta el orden en que colocamos la salida por la direccion del gradiente.
+            pesos += tasaAprendizaje * 0.5 * (salidaDeseada(i) - y) * patronesExt.row(i).t();
 
             // Graficar recta
             double pendiente = -pesos(1) / pesos(2);
@@ -127,29 +132,49 @@ pair<vec, double> entrenarPerceptron(const mat& patronesEnt,
                    << "plot " << gp.file1d(verdaderos) << "with points, "
                    << gp.file1d(falsos) << "with points, "
                    << gp.file1d(puntosRecta) << "with lines" << endl;
-
-                caracter = getchar();
             }
         }
         // Fin ciclo (época)
 
+        // Parte de prueba - Cálculo de la tasa de error
         int errores = 0;
         // Extender la matriz de patrones con la entrada correspondiente al umbral
-        const mat patronesPruebaExt = join_horiz(ones(patronesPrueba.n_rows) * (-1), patronesPrueba);
 
-        for (unsigned int i = 0; i < patronesPrueba.n_rows; ++i) {
-            double z = dot(patronesPruebaExt.row(i), pesos);
+        for (unsigned int i = 0; i < patronesExt.n_rows; ++i) {
+            double z = dot(patronesExt.row(i), pesos);
             int y = ic::sign(z);
 
-            if (y != salidaDeseadaPrueba(i))
+            if (y != salidaDeseada(i))
                 ++errores;
         }
 
-        tasaError = static_cast<double>(errores) / patronesPrueba.n_rows * 100;
+        tasaError = static_cast<double>(errores) / patrones.n_rows * 100;
         if (tasaError < tolerancia)
             break;
     }
     // Fin ciclo (epocas)
 
     return {pesos, tasaError};
+}
+
+double errorPrueba(const vec& pesos,
+                   const mat& patrones,
+                   const vec& salidaDeseada)
+{
+    // Se extiende la matriz de patrones con la entrada correspondiente al umbral
+    const mat patronesExt = join_horiz(ones(patrones.n_rows) * (-1), patrones);
+
+    int errores = 0;
+
+    for (unsigned int i = 0; i < patronesExt.n_rows; ++i) {
+        double z = dot(patronesExt.row(i), pesos);
+        int y = ic::sign(z);
+
+        if (y != salidaDeseada(i))
+            ++errores;
+    }
+
+    double tasaError = static_cast<double>(errores) / patronesExt.n_rows * 100;
+
+    return tasaError;
 }
