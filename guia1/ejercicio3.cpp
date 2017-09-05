84748a39f1a6e8f9407b4ad52a7855553ec075b7
Julián Unrrein <junrrein@gmail.com>
2017-09-05 14:44:37 -0300

    Guia 1 - Ejercicio 3: Entrenar la red con los datos

--- guia1/ejercicio3.cpp
+++ guia1/ejercicio3.cpp
@@ -4,8 +4,31 @@
 
 int main()
 {
+    arma_rng::set_seed_random();
+
     mat datos;
     datos.load(config::sourceDir + "/guia1/icgtp1datos/concentlite.csv");
+    //string rutaCarpeta = config::sourceDir + "/guia1/icgtp1datos/particionesConcent/";
+    //const vector<ic::Particion> particiones = ic::cargarParticiones(rutaCarpeta, 10);
+
+    ifstream ifs{config::sourceDir + "/guia1/parametrosConcentMulticapa.txt"};
+    ic::ParametrosMulticapa parametros;
+    if (!(ifs >> parametros))
+        throw runtime_error{"Error al leer los parámetros"};
+
+    vector<mat> pesos;
+    double tasaError;
+    int epoca;
+
+    tie(pesos, tasaError, epoca) = ic::entrenarMulticapa(parametros.estructuraRed,
+                                                         datos,
+                                                         parametros.nEpocas,
+                                                         parametros.tasaAprendizaje,
+                                                         parametros.parametroSigmoidea,
+                                                         parametros.toleranciaError);
+
+    cout << tasaError << endl
+         << epoca << endl;
 
     return 0;
 }
