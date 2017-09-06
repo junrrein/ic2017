80b19f720925b6b718cc35ad278e1cf5844808da
matyaspr <matyaspr@gmail.com>
2017-09-06 15:33:19 -0300

    Guia 1 - Ejercicio 4: Terminar ejercicio (parte del leave-one-out)

--- guia1/ejercicio4.cpp
+++ guia1/ejercicio4.cpp
@@ -15,35 +15,73 @@ int main()
     if (!(ifs >> parametros))
         throw runtime_error("No se pudo cargar el archivo de parámetros");
 
-    vector<ic::Particion> particiones = ic::cargarParticiones(rutaBaseDatos + "particionesIrisKOut/", 10);
-    vec errores;
-    errores.resize(particiones.size());
-    int noConvergio = 0;
+    // Leave K Out
 
-    for (unsigned int i = 0; i < particiones.size(); ++i) {
-        vector<mat> pesos;
-        int epocas;
-        tie(pesos, ignore, epocas) = ic::entrenarMulticapa(parametros.estructuraRed,
-                                                           datos.rows(particiones[i].first),
-                                                           parametros.nEpocas,
-                                                           parametros.tasaAprendizaje,
-                                                           parametros.inercia,
-                                                           parametros.parametroSigmoidea,
-                                                           parametros.toleranciaError);
+    {
+        vector<ic::Particion> particiones = ic::cargarParticiones(rutaBaseDatos + "particionesIrisKOut/", 10);
+        vec errores;
+        errores.resize(particiones.size());
+        int noConvergio = 0;
 
-        if (epocas == parametros.nEpocas)
-            ++noConvergio;
+        for (unsigned int i = 0; i < particiones.size(); ++i) {
+            vector<mat> pesos;
+            int epocas;
+            tie(pesos, ignore, epocas) = ic::entrenarMulticapa(parametros.estructuraRed,
+                                                               datos.rows(particiones[i].first),
+                                                               parametros.nEpocas,
+                                                               parametros.tasaAprendizaje,
+                                                               parametros.inercia,
+                                                               parametros.parametroSigmoidea,
+                                                               parametros.toleranciaError);
 
-        const double tasaError = ic::errorPrueba(pesos,
-                                                 datos.rows(particiones[i].second),
-                                                 parametros.parametroSigmoidea);
-        errores[i] = tasaError;
+            if (epocas == parametros.nEpocas)
+                ++noConvergio;
+
+            const double tasaError = ic::errorPrueba(pesos,
+                                                     datos.rows(particiones[i].second),
+                                                     parametros.parametroSigmoidea);
+            errores[i] = tasaError;
+        }
+
+        cout << "Iris multicapa, Leave K Out" << endl
+             << "Error promedio: " << mean(errores) << endl
+             << "Desvío estándar del error: " << stddev(errores) << endl
+             << "El algoritmo no convergió en " << noConvergio << " casos" << endl;
     }
 
-    cout << "Iris multicapa, Leave K Out" << endl
-         << "Error promedio: " << mean(errores) << endl
-         << "Desvío estándar del error: " << stddev(errores) << endl
-         << "El algoritmo no convergió en " << noConvergio << " casos" << endl;
+    // Leave One Out
+
+    {
+        vector<ic::Particion> particiones = ic::cargarParticiones(rutaBaseDatos + "particionesIris1Out/", 150);
+        vec errores;
+        errores.resize(particiones.size());
+        int noConvergio = 0;
+
+        for (unsigned int i = 0; i < particiones.size(); ++i) {
+            vector<mat> pesos;
+            int epocas;
+            tie(pesos, ignore, epocas) = ic::entrenarMulticapa(parametros.estructuraRed,
+                                                               datos.rows(particiones[i].first),
+                                                               parametros.nEpocas,
+                                                               parametros.tasaAprendizaje,
+                                                               parametros.inercia,
+                                                               parametros.parametroSigmoidea,
+                                                               parametros.toleranciaError);
+
+            if (epocas == parametros.nEpocas)
+                ++noConvergio;
+
+            const double tasaError = ic::errorPrueba(pesos,
+                                                     datos.rows(particiones[i].second),
+                                                     parametros.parametroSigmoidea);
+            errores[i] = tasaError;
+        }
+
+        cout << "\nIris multicapa, Leave One Out" << endl
+             << "Error promedio: " << mean(errores) << endl
+             << "Desvío estándar del error: " << stddev(errores) << endl
+             << "El algoritmo no convergió en " << noConvergio << " casos" << endl;
+    }
 
     return 0;
 }
