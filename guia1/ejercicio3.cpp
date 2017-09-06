c1105e02fb3b923a8e2183deb1faa4975fdb5206
matyaspr <matyaspr@gmail.com>
2017-09-06 14:01:11 -0300

    Guia 1 - Ejercicio 3: Implementar la parte c) (reducir dimensión de los datos y entrenar un perceptrón simple)

--- guia1/ejercicio3.cpp
+++ guia1/ejercicio3.cpp
@@ -3,6 +3,12 @@
 #include "../config.hpp"
 #include <gnuplot-iostream.h>
 
+mat reducirDimension(const mat& datos);
+tuple<vec, double, int> entrenarPerceptron(const mat& datos,
+                                           int nEpocas,
+                                           double tasaAprendizaje,
+                                           double toleranciaError);
+
 int main()
 {
     arma_rng::set_seed_random();
@@ -11,6 +17,8 @@ int main()
     datos.load(config::sourceDir + "/guia1/icgtp1datos/concentlite.csv");
     //string rutaCarpeta = config::sourceDir + "/guia1/icgtp1datos/particionesConcent/";
     //const vector<ic::Particion> particiones = ic::cargarParticiones(rutaCarpeta, 10);
+
+    // Parte a: Entrenar una red multicapa para clasificar los patrones
 
     ifstream ifs{config::sourceDir + "/guia1/parametrosConcentMulticapa.txt"};
     ic::ParametrosMulticapa parametros;
@@ -55,7 +63,7 @@ int main()
         }
     }
 
-    // Parte b: Experimentar qué pasa al usar el término de inercia
+    // Parte b : Experimentar qué pasa al usar el término de inercia
     // en el algoritmo de ajuste de pesos
 
     ifs.close();
@@ -63,31 +71,158 @@ int main()
     if (!(ifs >> parametros))
         throw runtime_error{"Error al leer los parámetros"};
 
-    tie(pesos, tasaError, epoca) = ic::entrenarMulticapa(parametros.estructuraRed,
-                                                         datos,
-                                                         parametros.nEpocas,
-                                                         parametros.tasaAprendizaje,
-                                                         parametros.inercia,
-                                                         parametros.parametroSigmoidea,
-                                                         parametros.toleranciaError);
+    tie(ignore, tasaError, epoca) = ic::entrenarMulticapa(parametros.estructuraRed,
+                                                          datos,
+                                                          parametros.nEpocas,
+                                                          parametros.tasaAprendizaje,
+                                                          parametros.inercia,
+                                                          parametros.parametroSigmoidea,
+                                                          parametros.toleranciaError);
 
-    cout << "Multicapa con inercia " << parametros.inercia << endl
+    cout << "\nMulticapa con inercia " << parametros.inercia << endl
          << "Tasa de error: " << tasaError << endl
          << "N° de epocas de entrenamiento: " << epoca << endl;
 
-    //    Gnuplot gp;
+    // Parte c: Convertir los patrones a una sola dimensión, obtenida como
+    // la distancia de cada patrón a la media total.
+    // Luego entrenar con un perceptrón simple y comparar resultados con lo anterior.
 
-    //    gp << "set title 'Clasificación de patrones' font ',13'\n"
-    //       << "set xlabel 'x_1'\n"
-    //       << "set ylabel 'x_2'\n"
-    //       << "set grid\n"
-    //       << "set pointsize 2\n"
-    //       << "plot " << gp.file1d(verdaderosPositivos) << "title 'Verdaderos Positivos' with points lt rgb 'blue', "
-    //       << gp.file1d(verdaderosNegativos) << "title 'Verdaderos Negativos' with points lt rgb 'red', "
-    //       << gp.file1d(falsosPositivos) << "title 'Falsos Positivos' with points lt rgb 'red' ps 1.5 pt 5, "
-    //       << gp.file1d(falsosNegativos) << "title 'Falsos Negativos' with points lt rgb 'blue' ps 1.5 pt 5" << endl;
+    const mat datosReducidos = reducirDimension(datos);
+    tie(ignore, tasaError, epoca) = entrenarPerceptron(datosReducidos, 500, 0.01, 4);
 
-    //    getchar();
+    cout << "\nPerceptrón simple en datos con dimensión reducida" << endl
+         << "Tasa de error: " << tasaError << endl
+         << "Epocas de entrenamiento: " << epoca << endl;
+
+    // Graficar los resultados de clasificación de la primer red multicapa
+
+    Gnuplot gp;
+
+    gp << "set title 'Clasificación de patrones' font ',13'\n"
+       << "set xlabel 'x_1' font ',11'\n"
+       << "set ylabel 'x_2' font ',11'\n"
+       << "set grid\n"
+       << "set pointsize 2\n"
+       << "plot " << gp.file1d(verdaderosPositivos) << "title 'Verdaderos Positivos' with points lt rgb 'blue', "
+       << gp.file1d(verdaderosNegativos) << "title 'Verdaderos Negativos' with points lt rgb 'red', "
+       << gp.file1d(falsosPositivos) << "title 'Falsos Positivos' with points lt rgb 'red' ps 1.5 pt 5, "
+       << gp.file1d(falsosNegativos) << "title 'Falsos Negativos' with points lt rgb 'blue' ps 1.5 pt 5" << endl;
+
+    getchar();
 
     return 0;
+}
+
+mat reducirDimension(const mat& datos)
+{
+    const mat patrones = datos.head_cols(2);
+    const vec salidaDeseada = datos.tail_cols(1);
+    const rowvec media = mean(patrones);
+    vec distancias;
+    distancias.resize(patrones.n_rows);
+
+    for (unsigned int i = 0; i < patrones.n_rows; ++i) {
+        const double distancia = norm(media - patrones.row(i));
+        distancias(i) = distancia;
+    }
+
+    return join_horiz(distancias, salidaDeseada);
+}
+
+namespace ic {
+int sign(double numero)
+{
+    if (numero >= 0)
+        return 1;
+    else
+        return -1;
+}
+}
+
+pair<vec, double> epocaPerceptron(const mat& patronesExt,
+                                  const vec& salidaDeseada,
+                                  double tasaAprendizaje,
+                                  const vec& pesos)
+{
+    //Entrenamiento
+    vec nuevosPesos = pesos;
+
+    for (unsigned int i = 0; i < patronesExt.n_rows; ++i) {
+        double z = dot(patronesExt.row(i), pesos);
+        int y = ic::sign(z);
+
+        // Actualizar pesos
+        nuevosPesos += tasaAprendizaje * (salidaDeseada(i) - y) * patronesExt.row(i).t();
+    } // Fin ciclo (Entrenamiento)
+
+    // Validacion
+    int errores = 0;
+
+    for (unsigned int i = 0; i < patronesExt.n_rows; ++i) {
+        double z = dot(patronesExt.row(i), pesos);
+        int y = ic::sign(z);
+
+        if (y != salidaDeseada(i))
+            ++errores;
+    }
+
+    double tasaError = static_cast<double>(errores) / patronesExt.n_rows * 100;
+
+    return {nuevosPesos, tasaError};
+} // fin funcion Epoca
+
+double errorPrueba(const vec& pesos,
+                   const mat& patrones,
+                   const vec& salidaDeseada)
+{
+    // Se extiende la matriz de patrones con la entrada correspondiente al umbral
+    const mat patronesExt = join_horiz(ones(patrones.n_rows) * (-1), patrones);
+
+    int errores = 0;
+
+    for (unsigned int i = 0; i < patronesExt.n_rows; ++i) {
+        double z = dot(patronesExt.row(i), pesos);
+        int y = ic::sign(z);
+
+        if (y != salidaDeseada(i))
+            ++errores;
+    }
+
+    double tasaError = static_cast<double>(errores) / patronesExt.n_rows * 100;
+
+    return tasaError;
+}
+
+tuple<vec, double, int> entrenarPerceptron(const mat& datos,
+                                           int nEpocas,
+                                           double tasaAprendizaje,
+                                           double toleranciaError)
+{
+    const vec salidaDeseada = datos.tail_cols(1);
+    // Extender la matriz de patrones con la entrada correspondiente al umbral
+    const int nParametros = datos.n_cols - 1;
+    const mat patronesExt = join_horiz(ones(datos.n_rows) * (-1), datos.head_cols(nParametros));
+
+    // Inicializar pesos y tasa de error
+    vec pesos = randu<vec>(patronesExt.n_cols) - 0.5;
+    double tasaError = 0;
+
+    // Ciclo de las epocas
+    int epoca = 1;
+    for (; epoca <= nEpocas; ++epoca) {
+        // Ciclo para una época
+        tie(pesos, tasaError) = epocaPerceptron(patronesExt,
+                                                salidaDeseada,
+                                                tasaAprendizaje,
+                                                pesos);
+
+        if (tasaError < toleranciaError)
+            break;
+    }
+    // Fin ciclo (epocas)
+
+    if (epoca > nEpocas)
+        epoca = nEpocas;
+
+    return make_tuple(pesos, tasaError, epoca);
 }
