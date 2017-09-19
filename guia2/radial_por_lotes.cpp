7e506d8100fc56b4ae466d263ff6f05f84aeddcf
matyaspr <matyaspr@gmail.com>
2017-09-19 16:04:10 -0300

    Guia 2: Agregar cosas faltantes de la guia

---
+++ guia2/radial_por_lotes.cpp
@@ -0,0 +1,14 @@
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+namespace ic {
+
+pair<vec, double> entrenarRadialPorLotes(const mat& patrones,
+                                         int nConjuntos,
+                                         int nEpocas)
+{
+	vector<vec> conjuntos;
+}
+}
