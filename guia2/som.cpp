951445da7e81ecfb6ca59e694dbc33284c5b6100
Julián Unrrein <junrrein@gmail.com>
2017-10-03 16:55:23 -0300

    Guia 1 - SOM: Agregar facilidades para etiquetar las neuronas y clasificar patrones

--- guia2/som.cpp
+++ guia2/som.cpp
@@ -9,7 +9,7 @@ namespace ic {
 class SOM {
 public:
     // Constructores
-    SOM(const mat& patrones, pair<int, int> dimensiones);
+    SOM(const mat& patrones, pair<int, int> dimensiones, const vec& salidaDeseada = {});
 
     // Interfaz
     void entrenar(int nEpocas,
@@ -17,19 +17,28 @@ public:
                   double velocidadFinal,
                   int vecindadInicial,
                   int vecindadFinal);
+    void etiquetar();
+    int clasificar(const rowvec& patron) const;
+    vec clasificar(const mat& patrones) const;
     void graficar(Gnuplot& gp) const;
 
     // Acceso a miembros
     const field<rowvec>& mapa() const { return m_mapa; };
+    const mat& etiquetas() const { return m_etiquetas; };
     const mat& patrones() const { return m_patrones; };
 
 private:
     field<rowvec> m_mapa;
+    mat m_etiquetas;
     const mat m_patrones;
+    const vec m_salidaDeseada;
+
+    pair<pair<int, int>, double> buscarGanadora(const rowvec& patron) const;
 };
 
-SOM::SOM(const mat& patrones, pair<int, int> dimensiones)
+SOM::SOM(const mat& patrones, pair<int, int> dimensiones, const vec& salidaDeseada)
     : m_patrones{patrones}
+    , m_salidaDeseada{salidaDeseada}
 {
     m_mapa = field<rowvec>(dimensiones.first, dimensiones.second);
 
@@ -39,6 +48,25 @@ SOM::SOM(const mat& patrones, pair<int, int> dimensiones)
             m_mapa(i, j) = randu<rowvec>(patrones.n_cols) - 0.5;
         }
     }
+}
+
+pair<pair<int, int>, double> SOM::buscarGanadora(const rowvec& patron) const
+{
+    pair<int, int> coordGanadora;
+    double distanciaGanadora = numeric_limits<double>::max();
+
+    for (unsigned int j = 0; j < m_mapa.n_rows; ++j) {
+        for (unsigned int k = 0; k < m_mapa.n_cols; ++k) {
+            const double distancia = norm(patron - m_mapa(j, k));
+
+            if (distancia < distanciaGanadora) {
+                distanciaGanadora = distancia;
+                coordGanadora = {j, k};
+            }
+        }
+    }
+
+    return {coordGanadora, distanciaGanadora};
 }
 
 void SOM::entrenar(int nEpocas,
@@ -56,18 +84,8 @@ void SOM::entrenar(int nEpocas,
 
             // Buscamos la neurona ganadora
             pair<int, int> coordGanadora;
-            double distanciaGanadora = numeric_limits<double>::max();
-
-            for (unsigned int j = 0; j < m_mapa.n_rows; ++j) {
-                for (unsigned int k = 0; k < m_mapa.n_cols; ++k) {
-                    const double distancia = norm(m_patrones.row(n) - m_mapa(j, k));
-
-                    if (distancia < distanciaGanadora) {
-                        distanciaGanadora = distancia;
-                        coordGanadora = {j, k};
-                    }
-                }
-            }
+            double distanciaGanadora;
+            tie(coordGanadora, distanciaGanadora) = buscarGanadora(m_patrones.row(n));
 
             // Adaptación de pesos
             const int maxX = int(m_mapa.n_rows - 1);
@@ -84,6 +102,63 @@ void SOM::entrenar(int nEpocas,
             }
         }
     }
+}
+
+void SOM::etiquetar()
+{
+    if (m_salidaDeseada.empty())
+        throw runtime_error("Este SOM no posee una salida deseada asociada a los patrones");
+
+    field<vec> mapaContador(m_mapa.n_rows, m_mapa.n_cols);
+
+    // Inicializar los contadores de clases
+    for (unsigned int x = 0; x < m_mapa.n_rows; ++x) {
+        for (unsigned int y = 0; y < m_mapa.n_cols; ++y) {
+            mapaContador(x, y) = zeros(2);
+        }
+    }
+
+    // Contamos, para cada neurona, qué cantidad de veces gana para cada clase
+    for (unsigned int n = 0; n < m_patrones.n_rows; ++n) {
+        pair<int, int> ganadora;
+        tie(ganadora, ignore) = buscarGanadora(m_patrones.row(n));
+
+        if (m_salidaDeseada(n) == 0)
+            mapaContador(ganadora.first, ganadora.second).at(0) += 1;
+        else
+            mapaContador(ganadora.first, ganadora.second).at(1) += 1;
+    }
+
+    m_etiquetas = mat(m_mapa.n_rows, m_mapa.n_cols);
+
+    // Se asignan las etiquetas de clase
+    for (unsigned int x = 0; x < m_mapa.n_rows; ++x) {
+        for (unsigned int y = 0; y < m_mapa.n_cols; ++y) {
+            if (max(mapaContador(x, y)) == min(mapaContador(x, y)))
+                throw runtime_error("La neurona se activó el mismo nro de veces para las dos clases");
+
+            m_etiquetas(x, y) = index_max(mapaContador(x, y));
+        }
+    }
+}
+
+int SOM::clasificar(const rowvec& patron) const
+{
+    pair<int, int> ganadora;
+    tie(ganadora, ignore) = buscarGanadora(patron);
+
+    return m_etiquetas(ganadora.first, ganadora.second);
+}
+
+vec SOM::clasificar(const mat& patrones) const
+{
+    vec result = zeros(patrones.n_rows);
+
+    for (unsigned int n = 0; n < patrones.n_rows; ++n) {
+        result(n) = clasificar(rowvec{patrones.row(n)});
+    }
+
+    return result;
 }
 
 void SOM::graficar(Gnuplot& gp) const
@@ -110,16 +185,6 @@ void SOM::graficar(Gnuplot& gp) const
                 gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x, y - 1)).eval()) << "notitle with lines lt -1, ";
             if (y != m_mapa.n_cols - 1)
                 gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x, y + 1)).eval()) << "notitle with lines lt -1, ";
-
-            // Graficar conexiones con las vecinas diagonales
-            if (x != 0 && y != 0)
-                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x - 1, y - 1)).eval()) << "notitle with lines lt -1, ";
-            if (x != 0 && y != m_mapa.n_cols - 1)
-                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x - 1, y + 1)).eval()) << "notitle with lines lt -1, ";
-            if (x != m_mapa.n_rows - 1 && y != 0)
-                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x + 1, y - 1)).eval()) << "notitle with lines lt -1, ";
-            if (x != m_mapa.n_rows - 1 && y != m_mapa.n_cols - 1)
-                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x + 1, y + 1)).eval()) << "notitle with lines lt -1, ";
         }
     }
 
