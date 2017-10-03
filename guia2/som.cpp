fffa00f16ad71f8f433345148e0a30e12556e551
Julián Unrrein <junrrein@gmail.com>
2017-10-03 10:51:03 -0300

    Guia 2 - SOM: Terminar de programar el entrenamiento. Agregar función para graficar el mapa. Corregir errores

--- guia2/som.cpp
+++ guia2/som.cpp
@@ -1,4 +1,5 @@
 #include <armadillo>
+#include <gnuplot-iostream.h>
 
 using namespace arma;
 using namespace std;
@@ -14,6 +15,7 @@ public:
                   double velocidadFinal,
                   int vecindadInicial,
                   int vecindadFinal);
+    void graficar(Gnuplot& gp) const;
 
     // Acceso a miembros
     const field<rowvec>& mapa() const { return m_mapa; };
@@ -32,7 +34,7 @@ SOM::SOM(const mat& patrones, pair<int, int> dimensiones)
     // Inicializar mapa
     for (unsigned int i = 0; i < m_mapa.n_rows; ++i) {
         for (unsigned int j = 0; j < m_mapa.n_cols; ++j) {
-            m_mapa(i, j) = randu(patrones.n_cols) - 0.5;
+            m_mapa(i, j) = randu<rowvec>(patrones.n_cols) - 0.5;
         }
     }
 }
@@ -43,7 +45,11 @@ void SOM::entrenar(int nEpocas,
                    int vecindadInicial,
                    int vecindadFinal)
 {
-    for (int i = 0; i < nEpocas; ++i) {
+    const vec velocidad = linspace(velocidadInicial, velocidadFinal, nEpocas);
+    // Redondeamos la vecindad porque necesitamos que tenga valores enteros
+    const vec vecindad = round(linspace(vecindadInicial, vecindadFinal, nEpocas));
+
+    for (int epoca = 0; epoca < nEpocas; ++epoca) {
         for (unsigned int n = 0; n < m_patrones.n_rows; ++n) {
 
             // Buscamos la neurona ganadora
@@ -51,7 +57,7 @@ void SOM::entrenar(int nEpocas,
             double distanciaGanadora = numeric_limits<double>::max();
 
             for (unsigned int j = 0; j < m_mapa.n_rows; ++j) {
-                for (unsigned int k = 0; k < m_mapa.n_rows; ++k) {
+                for (unsigned int k = 0; k < m_mapa.n_cols; ++k) {
                     const double distancia = norm(m_patrones.row(n) - m_mapa(j, k));
 
                     if (distancia < distanciaGanadora) {
@@ -62,19 +68,55 @@ void SOM::entrenar(int nEpocas,
             }
 
             // Adaptación de pesos
-            int vecindad = 1;
-            double velocidad = 0.2;
-
-            const int xInicial = (coordGanadora.first - vecindad < 0) ? 0 : (coordGanadora.first - vecindad);
-            const int xFinal = (coordGanadora.first + vecindad > m_mapa.n_cols) ? m_mapa.n_cols : (coordGanadora.first + vecindad);
-            const int yInicial = (coordGanadora.second - vecindad < 0) ? 0 : (coordGanadora.second - vecindad);
-            const int yFinal = (coordGanadora.second + vecindad > m_mapa.n_rows) ? m_mapa.n_rows : (coordGanadora.second + vecindad);
+            const int maxX = int(m_mapa.n_rows - 1);
+            const int maxY = int(m_mapa.n_cols - 1);
+            const int xInicial = (coordGanadora.first - vecindad(epoca) < 0) ? 0 : (coordGanadora.first - vecindad(epoca));
+            const int xFinal = (coordGanadora.first + vecindad(epoca) > maxX) ? maxX : (coordGanadora.first + vecindad(epoca));
+            const int yInicial = (coordGanadora.second - vecindad(epoca) < 0) ? 0 : (coordGanadora.second - vecindad(epoca));
+            const int yFinal = (coordGanadora.second + vecindad(epoca) > maxY) ? maxY : (coordGanadora.second + vecindad(epoca));
 
             for (int x = xInicial; x <= xFinal; ++x) {
                 for (int y = yInicial; y <= yFinal; ++y) {
-                    m_mapa(x, y) += velocidad * distanciaGanadora;
+                    m_mapa(x, y) += velocidad(epoca) * (m_patrones.row(n) - m_mapa(x, y));
                 }
             }
         }
     }
+}
+
+void SOM::graficar(gnuplotio::Gnuplot& gp) const
+{
+    gp << "set key box opaque width 3" << endl
+       << "set xlabel 'x_1' font ',11'" << endl
+       << "set ylabel 'x_2' font ',11'" << endl
+       << "plot ";
+    // Graficar neuronas del mapa
+    for (unsigned int x = 0; x < m_mapa.n_rows; ++x) {
+        for (unsigned int y = 0; y < m_mapa.n_cols; ++y) {
+            gp << gp.file1d(m_mapa(x, y).eval()) << "notitle with points ps 2 pt 1 lt -1 lw 3, ";
+
+            // Graficar conexiones con las vecinas
+            if (x != 0)
+                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x - 1, y)).eval()) << "notitle with lines lt -1, ";
+            if (x != m_mapa.n_rows - 1)
+                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x + 1, y)).eval()) << "notitle with lines lt -1, ";
+            if (y != 0)
+                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x, y - 1)).eval()) << "notitle with lines lt -1, ";
+            if (y != m_mapa.n_cols - 1)
+                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x, y + 1)).eval()) << "notitle with lines lt -1, ";
+            // Graficar conexiones con las vecinas diagonales
+            if (x != 0 && y != 0)
+                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x - 1, y - 1)).eval()) << "notitle with lines lt -1, ";
+            if (x != 0 && y != m_mapa.n_cols - 1)
+                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x - 1, y + 1)).eval()) << "notitle with lines lt -1, ";
+            if (x != m_mapa.n_rows - 1 && y != 0)
+                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x + 1, y - 1)).eval()) << "notitle with lines lt -1, ";
+            if (x != m_mapa.n_rows - 1 && y != m_mapa.n_cols - 1)
+                gp << gp.file1d(join_vert(m_mapa(x, y), m_mapa(x + 1, y + 1)).eval()) << "notitle with lines lt -1, ";
+        }
+    }
+
+    // Graficar patrones
+    gp << gp.file1d(m_patrones) << "title 'Patrones' with points pt 2 ps 1, "
+       << "NaN title 'Neuronas' with points ps 2 pt 1 lt -1 lw 3" << endl;
 }
