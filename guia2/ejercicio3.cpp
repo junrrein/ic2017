bdecc0b4891931658d26544c68d61d1202f466c2
Julián Unrrein <junrrein@gmail.com>
2017-10-05 11:02:09 -0300

    Guia 2 - Ejercicio 3: Aumentar la cantidad de neuronas usadas. Mejorar gráficas

--- guia2/ejercicio3.cpp
+++ guia2/ejercicio3.cpp
@@ -12,11 +12,12 @@ int main()
     datos.load(config::sourceDir + "/guia2/datos/circulo.csv");
 
     // Mapa 2D
-    ic::SOM somCirculo2D{datos, {5, 4}};
+    ic::SOM somCirculo2D{datos, {6, 6}};
     Gnuplot gp;
 
     somCirculo2D.entrenar(500, 0.7, 0.7, 2, 2);
-    gp << "set title 'SOM 2D para circulo.csv - Etapa de ordenamiento topológico' font ',12'" << endl;
+    gp << "set terminal qt size 600, 450" << endl
+       << "set title 'SOM 2D para circulo.csv - Etapa de ordenamiento topológico' font ',12'" << endl;
     somCirculo2D.graficar(gp);
     getchar();
 
@@ -31,14 +32,14 @@ int main()
     getchar();
 
     // Mapa 1D
-    ic::SOM somCirculo1D{datos, {20, 1}};
+    ic::SOM somCirculo1D{datos, {36, 1}};
 
     somCirculo1D.entrenar(500, 0.7, 0.7, 5, 5);
     gp << "set title 'SOM 1D para circulo.csv - Etapa de ordenamiento topológico'" << endl;
     somCirculo1D.graficar(gp);
     getchar();
 
-    somCirculo1D.entrenar(1000, 0.7, 0.1, 7, 1);
+    somCirculo1D.entrenar(1000, 0.7, 0.1, 5, 1);
     gp << "set title 'SOM 1D para circulo.csv - Etapa de transición'" << endl;
     somCirculo1D.graficar(gp);
     getchar();
@@ -48,16 +49,26 @@ int main()
     somCirculo1D.graficar(gp);
     getchar();
 
+    gp << "set terminal qt size 1200, 450" << endl
+       << "set multiplot layout 1,2" << endl
+       << "set title 'SOM 2D para circulo.csv'" << endl;
+    somCirculo2D.graficar(gp, false);
+    gp << "set title 'SOM 1D para circulo.csv'" << endl;
+    somCirculo1D.graficar(gp, false);
+    getchar();
+    gp << "unset multiplot" << endl;
+
     // -----------------------------
     //           te.csv
     // -----------------------------
     datos.load(config::sourceDir + "/guia2/datos/te.csv");
 
     // Mapa 2D
-    ic::SOM somTe2D{datos, {2, 8}};
+    ic::SOM somTe2D{datos, {6, 6}};
 
     somTe2D.entrenar(500, 0.7, 0.7, 2, 2);
-    gp << "set title 'SOM 2D para te.csv - Etapa de ordenamiento topológico'" << endl
+    gp << "set terminal qt size 600, 450" << endl
+       << "set title 'SOM 2D para te.csv - Etapa de ordenamiento topológico'" << endl
        << "set xrange [-1.5:1.5]" << endl
        << "set yrange [-1.5:1.5]" << endl;
     somTe2D.graficar(gp);
@@ -74,14 +85,14 @@ int main()
     getchar();
 
     // Mapa 1D
-    ic::SOM somTe1D{datos, {16, 1}};
+    ic::SOM somTe1D{datos, {36, 1}};
 
-    somTe1D.entrenar(500, 0.7, 0.7, 4, 4);
+    somTe1D.entrenar(500, 0.7, 0.7, 5, 5);
     gp << "set title 'SOM 1D para te.csv - Etapa de ordenamiento topológico'" << endl;
     somTe1D.graficar(gp);
     getchar();
 
-    somTe1D.entrenar(1000, 0.7, 0.1, 4, 1);
+    somTe1D.entrenar(1000, 0.7, 0.1, 5, 1);
     gp << "set title 'SOM 1D para te.csv - Etapa de transición'" << endl;
     somTe1D.graficar(gp);
     getchar();
@@ -89,6 +100,14 @@ int main()
     somTe1D.entrenar(3000, 0.01, 0.01, 0, 0);
     gp << "set title 'SOM 1D para te.csv - Etapa de ajuste fino'" << endl;
     somTe1D.graficar(gp);
+    getchar();
+
+    gp << "set terminal qt size 1200, 450" << endl
+       << "set multiplot layout 1,2" << endl
+       << "set title 'SOM 2D para te.csv'" << endl;
+    somTe2D.graficar(gp, false);
+    gp << "set title 'SOM 1D para te.csv'" << endl;
+    somTe1D.graficar(gp, false);
     getchar();
 
     return 0;
