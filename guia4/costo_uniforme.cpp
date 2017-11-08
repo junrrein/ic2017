8a49bbfefa6c62fad2e8a5c10202614e1a75f1ac
Julián Unrrein <junrrein@gmail.com>
2017-11-08 17:38:26 -0300

    Guia 4 - Ejercicio 3: Terminar de implementar y resolver el ejercicio usando búsqueda de costo uniforme

--- guia4/costo_uniforme.cpp
+++ guia4/costo_uniforme.cpp
@@ -5,6 +5,7 @@
 
 using namespace arma;
 using namespace std;
+using boost::algorithm::none_of_equal;
 
 struct Camino {
     int ciudadFinal;
@@ -17,14 +18,26 @@ bool operator<(const Camino& lhs, const Camino& rhs)
     return lhs.costoCamino < rhs.costoCamino;
 }
 
+ostream& operator<<(ostream& os, const Camino& camino)
+{
+    os << "Recorrido: ";
+
+    for (int ciudad : camino.recorrido)
+        os << ciudad << ' ';
+
+    os << "\nCosto del camino: " << camino.costoCamino << endl;
+
+    return os;
+}
+
 class ArbolBusqueda {
 public:
     ArbolBusqueda(string rutaArchivoDistancias);
 
-    vector<int> hacerBusqueda();
+    Camino hacerBusqueda();
 
 private:
-    set<Camino> listaCaminos;
+    multiset<Camino> listaCaminos;
     unsigned int cantidadCiudades;
     mat distancias;
 };
@@ -38,15 +51,23 @@ ArbolBusqueda::ArbolBusqueda(string rutaArchivoDistancias)
 
     cantidadCiudades = distancias.n_rows;
 
-    Camino nodoInicial;
-    nodoInicial.ciudadFinal = randi(1, distr_param(1, cantidadCiudades)).at(0);
-    nodoInicial.recorrido = {nodoInicial.ciudadFinal};
-    nodoInicial.costoCamino = 0;
+    // Insertar caminos iniciales que empiecen en las distintas ciudades
+    // Los insertamos de forma desordenada
+    vector<int> ciudades(cantidadCiudades);
+    iota(ciudades.begin(), ciudades.end(), 1);
+    random_shuffle(ciudades.begin(), ciudades.end());
 
-    listaCaminos.insert(nodoInicial);
+    for (int i : ciudades) {
+        Camino puntoPartida;
+        puntoPartida.ciudadFinal = i;
+        puntoPartida.recorrido = {puntoPartida.ciudadFinal};
+        puntoPartida.costoCamino = 0;
+
+        listaCaminos.insert(puntoPartida);
+    }
 }
 
-vector<int> ArbolBusqueda::hacerBusqueda()
+Camino ArbolBusqueda::hacerBusqueda()
 {
     while (!listaCaminos.empty()) {
         //  1 - Saco el primer elemento de la lista
@@ -57,34 +78,49 @@ vector<int> ArbolBusqueda::hacerBusqueda()
         //  2 - Me fijo si es un estado objetivo
         //      Si es un estado objetivo, encontré la solución
         // El camino va a ser la solución si la cantidad de nodos es igual
-        // a la cantidad de ciudades.
-        if (camino.recorrido.size() == cantidadCiudades)
-            return camino.recorrido;
+        // a la cantidad de ciudades + 1 (porque después de pasar por todas las
+        // ciudades, tiene que volver al punto de partida).
+        if (camino.recorrido.size() == cantidadCiudades + 1)
+            return camino;
 
+        // Si el recorrido ya tiene nCiudades, entonces ya recorrió todas,
+        // y solo le falta volver a la ciudad inicial.
+        // Este camino camino va a ser una posible solución.
+        if (camino.recorrido.size() == cantidadCiudades) {
+            Camino posibleSolucion{camino};
+
+            posibleSolucion.ciudadFinal = posibleSolucion.recorrido.front();
+            posibleSolucion.recorrido.push_back(posibleSolucion.recorrido.front());
+            posibleSolucion.costoCamino += distancias.at(camino.ciudadFinal - 1,
+                                                         posibleSolucion.ciudadFinal - 1);
+
+            listaCaminos.insert(posibleSolucion);
+        }
         //  3 - Expando los vecinos a los que puede ir y los meto en listaCaminos.
         //      Los nuevos Caminos son insertados para que lista quede ordenada por
         //      el costo de cada camino.
         //      Si no hay vecinos a expandir, no inserto nada.
-        for (unsigned int ciudad = 1; ciudad <= cantidadCiudades; ++ciudad) {
+        else
+            for (unsigned int ciudad = 1; ciudad <= cantidadCiudades; ++ciudad) {
 
-            // Si la ciudad no está en el recorrido hecho, entonces puedo viajar
-            // a esa ciudad.
-            if (boost::algorithm::none_of_equal(camino.recorrido, ciudad)) {
+                // Si la ciudad no está en el recorrido hecho, entonces puedo viajar
+                // a esa ciudad.
+                if (none_of_equal(camino.recorrido, ciudad)) {
 
-                // Creo un nuevo camino en base al actual
-                Camino nuevoCamino{camino};
+                    // Creo un nuevo camino en base al actual
+                    Camino nuevoCamino{camino};
 
-                // Actualizo el nuevo camino con la información sobre la
-                // nueva ciudad a la que se viajó.
-                nuevoCamino.ciudadFinal = ciudad;
-                nuevoCamino.recorrido.push_back(ciudad);
-                nuevoCamino.costoCamino += distancias.at(camino.ciudadFinal - 1,
-                                                         nuevoCamino.ciudadFinal - 1);
+                    // Actualizo el nuevo camino con la información sobre la
+                    // nueva ciudad a la que se viajó.
+                    nuevoCamino.ciudadFinal = ciudad;
+                    nuevoCamino.recorrido.push_back(ciudad);
+                    nuevoCamino.costoCamino += distancias.at(camino.ciudadFinal - 1,
+                                                             nuevoCamino.ciudadFinal - 1);
 
-                // Inserto el nuevo camino
-                listaCaminos.insert(nuevoCamino);
+                    // Inserto el nuevo camino
+                    listaCaminos.insert(nuevoCamino);
+                }
             }
-        }
     }
 
     // Si se llegó hasta acá, es porque no se encontró una solución
--- guia4/ejercicio3.cpp
+++ guia4/ejercicio3.cpp
@@ -1,6 +1,20 @@
 #include "costo_uniforme.cpp"
+#include "../config.hpp"
 
 int main()
 {
+    arma_rng::set_seed_random();
+    srand(time(0));
+
+    const string rutaArchivo7 = config::sourceDir + "/guia4/datos/7cities.csv";
+    ArbolBusqueda a7{rutaArchivo7};
+
+    cout << a7.hacerBusqueda();
+
+    const string rutaArchivo10 = config::sourceDir + "/guia4/datos/10cities.csv";
+    ArbolBusqueda a10{rutaArchivo10};
+
+    cout << a10.hacerBusqueda();
+
     return 0;
 }
