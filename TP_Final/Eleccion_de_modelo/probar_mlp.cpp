f11b29c510dadbb966797ef97369f4525a3d6875
Julián Unrrein <junrrein@gmail.com>
2017-11-23 02:47:22 -0300

    TP Final: Elección de modelo: Probar el nuevo MLP para ventas

--- TP_Final/Eleccion_de_modelo/probar_mlp.cpp
+++ TP_Final/Eleccion_de_modelo/probar_mlp.cpp
@@ -10,22 +10,22 @@ int main()
 {
     const string rutaBase = config::sourceDir + "/TP_Final/datos/";
     const string rutaVentas = rutaBase + "Ventas.csv";
-    const string rutaDiferencias = rutaBase + "DiferenciasRelativas.csv";
     const string rutaExportaciones = rutaBase + "Exportaciones.csv";
 
+    const int nRetardos = 8;
+    const int nSalidas = 3;
     ParametrosMulticapa parametros;
-    parametros.estructuraRed = {23, 6};
+    parametros.estructuraRed = {24, nSalidas};
     parametros.nEpocas = 2000;
     parametros.tasaAprendizaje = 0.00075;
     parametros.inercia = 0.2;
     parametros.toleranciaError = 10;
 
     // Carga de datos
-    Particion particion = cargarTuplas({rutaDiferencias, rutaExportaciones},
+    Particion particion = cargarTuplas({rutaExportaciones},
                                        rutaVentas,
-                                       14,
-                                       6,
-                                       false);
+                                       nRetardos,
+                                       nSalidas);
 
     const mat datosEntrenamiento = join_vert(join_horiz(particion.entrenamiento.tuplasEntrada,
                                                         particion.entrenamiento.tuplasSalida),
@@ -44,14 +44,14 @@ int main()
 
     // Prueba
     // Calcular la salida para los datos de prueba
-    vector<vec> salidaRed(6, vec(particion.prueba.tuplasEntrada.n_rows));
+    vector<vec> salidaRed(nSalidas, vec(particion.prueba.tuplasEntrada.n_rows));
 
     for (unsigned int n = 0; n < particion.prueba.tuplasEntrada.n_rows; ++n) {
         vec salidas = ic::salidaMulticapa(pesos,
                                           particion.prueba.tuplasEntrada.row(n).t())
                           .back();
 
-        for (int j = 0; j < 6; ++j)
+        for (unsigned int j = 0; j < salidaRed.size(); ++j)
             salidaRed.at(j)(n) = salidas(j);
     }
 
@@ -65,9 +65,9 @@ int main()
     }
 
     // Calculo de error
-    vec promedioErrorPorMes(6);
-    vec desvioErrorPorMes(6);
-    for (int i = 0; i < 6; ++i) {
+    vec promedioErrorPorMes(nSalidas);
+    vec desvioErrorPorMes(nSalidas);
+    for (int i = 0; i < nSalidas; ++i) {
         const vec erroresRelativosAbsolutos = abs(particion.prueba.tuplasSalida.col(i) - salidaRed.at(i))
                                               / particion.prueba.tuplasSalida.col(i)
                                               * 100;
@@ -80,8 +80,8 @@ int main()
     errores.save(rutaBase + "errorMlpSalidaVentas.csv", arma::csv_ascii);
 
     Gnuplot gp;
-    gp << "set terminal qt size 1200,600" << endl
-       << "set multiplot layout 2,3 title 'Predicción usando Diferencias Relativas de Patentamientos + Exportaciones - Red MLP' font ',12'" << endl
+    gp << "set terminal qt size 600,700" << endl
+       << "set multiplot layout 3,1 title 'Predicción usando Exportaciones - Red MLP' font ',12'" << endl
        << "set xlabel 'Mes (final de la serie)'" << endl
        << "set ylabel 'Patentamientos (unidades)'" << endl
        << "set yrange [0:70000]" << endl
